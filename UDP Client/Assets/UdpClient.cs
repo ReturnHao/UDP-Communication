7822fce772aa957d6bf8f65aea516075ec710d93
ReturnHao <798273600@qq.com>
2019-02-27 11:07:23 +0900

    UDP Communication

---
+++ UDP Client/Assets/UdpClient.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UdpClient : MonoBehaviour
+{
+    string editString = "Hello Wolrd";
+
+    Socket aimSocket;
+    EndPoint serverEnd;
+    IPEndPoint ipEnd;
+    string recvStr;
+    string sendStr;
+    byte[] recvData = new byte[1024]; 
+    byte[] sendData = new byte[1024];
+    int recvLen;
+    Thread connectThread;
+
+    void InitSocket()
+    {
+        ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8001);
+        aimSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        serverEnd = (EndPoint)sender;
+        print("Waiting for sending UDP dgram");
+
+        SocketSend("Hello");
+
+        connectThread = new Thread(new ThreadStart(SocketReceive));
+        connectThread.Start();
+    }
+
+    void SocketSend(string _sendStr)
+    {
+        sendData = new byte[1024];
+        sendData = Encoding.ASCII.GetBytes(_sendStr);
+
+        aimSocket.SendTo(sendData, sendData.Length, SocketFlags.None, ipEnd);
+    }
+
+    void SocketReceive()
+    {
+        while (true)
+        {
+            recvData = new byte[1024];
+            recvLen = aimSocket.ReceiveFrom(recvData, ref serverEnd);
+            print("Message from: " + serverEnd.ToString()); 
+
+            recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
+            print(recvStr);
+        }
+    }
+
+    void SocketQuit()
+    {
+        if (connectThread != null)
+        {
+            connectThread.Interrupt();
+            connectThread.Abort();
+        }
+
+        if (aimSocket != null) aimSocket.Close();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        InitSocket();
+    }
+
+    void OnGUI()
+    {
+        editString = GUI.TextField(new Rect(10, 10, 100, 20), editString);
+        if (GUI.Button(new Rect(10, 30, 60, 20), "send"))
+            SocketSend(editString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnApplicationQuit()
+    {
+        SocketQuit();
+    }
+}
---
+++ UDP Server/Assets/UdpServer.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UdpServer : MonoBehaviour
+{
+    Socket aimSocket;
+    EndPoint clientEnd;
+    IPEndPoint ipEnd;
+    string recvStr;
+    string sendStr;
+    byte[] recvData = new byte[1024];
+    byte[] sendData = new byte[1024];
+    int recvLen;
+    Thread connectThread;
+
+    void SocketQuit()
+    {
+        if (connectThread != null)
+        {
+            connectThread.Interrupt();
+            connectThread.Abort();
+        }
+
+        if (aimSocket != null) aimSocket.Close();
+        print("Disconnected");
+    }
+
+
+    void SocketSend(string _sendStr)
+    {
+        sendData = new byte[1024];
+        sendData = Encoding.ASCII.GetBytes(_sendStr);
+
+        aimSocket.SendTo(sendData, sendData.Length, SocketFlags.None, clientEnd);
+    }
+
+    void SocketReceive()
+    {
+        while (true)
+        {
+            recvData = new byte[1024];
+            recvLen = aimSocket.ReceiveFrom(recvData, ref clientEnd);
+            print("Message from: " + clientEnd.ToString());
+
+            recvStr = Encoding.ASCII.GetString(recvData, 0, recvLen);
+            print(recvStr);
+
+            sendStr = "From Server: " + recvStr;
+            SocketSend(sendStr);
+        }
+    }
+
+    void InitSocket()
+    {
+        ipEnd = new IPEndPoint(IPAddress.Any, 8001);
+        aimSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        aimSocket.Bind(ipEnd);
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        clientEnd = (EndPoint)sender;
+        print("Waiting for a UDP dgram");
+
+        connectThread = new Thread(new ThreadStart(SocketReceive));
+        connectThread.Start();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitSocket();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        SocketQuit();
+    }
+}
